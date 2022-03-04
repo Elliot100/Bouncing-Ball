74dcb13eaebb040afd6901e1eb99aadd059a9b39
Elly <ellychau10@yahoo.com>
2022-03-04 13:44:46 -0800

    Basic mouse press effect

--- sketch.js
+++ sketch.js
@@ -2,8 +2,8 @@
 //setup variables
 ballx = 200;
 bally = 50;
-balldx = 0;
-balldy = 1;
+balldx = 1;
+balldy = 0;
 GRAVITY = 0.1;
 
 function setup() {
@@ -16,12 +16,23 @@ function draw() {
   //move ball
   balldy += GRAVITY;
   bally += balldy;
+  ballx += balldx;
 
   //bounce
-  if(bally>290)
+  if(bally > 290) {
     balldy *= -1;
+  }
+  if(ballx > 390 || ballx < 10) {
+    balldx *= -1;
+  }
 
   //draw ball
   fill(0,0,200);
   circle(ballx, bally, 20);
+
+  //interaction
+  if(mouseIsPressed) {
+    ballx = mouseX;
+    bally = mouseY;
+  }
 }
