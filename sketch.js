1155fc36c8cc3efa6234b1cc6ef9fdf3eba4c3bd
Elly <ellychau10@yahoo.com>
2022-03-04 13:29:16 -0800

    simple vertical bouncing ball

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ ballx = 200;
 bally = 50;
 balldx = 0;
 balldy = 1;
+GRAVITY = 0.1;
 
 function setup() {
   createCanvas(400,300);
@@ -13,7 +14,12 @@ function draw() {
   background(0, 200, 0);
 
   //move ball
+  balldy += GRAVITY;
   bally += balldy;
+
+  //bounce
+  if(bally>290)
+    balldy *= -1;
 
   //draw ball
   fill(0,0,200);
