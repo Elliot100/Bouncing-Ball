ba95f4cb48b248c26a435b4f2cc64089cd40b2d7
Elly <ellychau10@yahoo.com>
2022-03-08 15:48:24 -0800

    cursor rectangle to bounce off balls that land on it

--- sketch.js
+++ sketch.js
@@ -2,8 +2,8 @@
 //setup variables
 var wx = window.innerWidth;
 var wy = window.innerHeight;
-const GRAVITY = 0.99;
-const ACC = 1.2;
+const GRAVITY = 1;
+const ACC = 1.5;
 const colors = ["lime","cyan","pink","yellow","fuchsia","orange"];
 
 
@@ -27,12 +27,14 @@ function setup() {
 
 function Ball() {
   this.color = colors[Math.floor(Math.random() * colors.length)];
-  this.radius = Math.random() * 20 + 14;
+  this.radius = 20;
+  // this.radius = Math.random() * 20 + 14;
   this.x = Math.random() * (wx - this.radius * 2) + this.radius;
   this.y = Math.random() * (wy - this.radius);
   this.dy = Math.random() * 2;
   this.dx = Math.round((Math.random() - 0.5) * 10);
-  this.vel = Math.random() / 5;
+  this.vel = 0.5;
+  // this.vel = Math.random() / 5;
 
   this.update = function() {
     fill(this.color);
@@ -42,22 +44,41 @@ function Ball() {
 }
 
 var bal = [];
-for (var i=0; i<30; i++) {
+for (var i=0; i<50; i++) {
   bal.push(new Ball());
 }
 
-function mousepressed(x, y) {
-  for (var i = 0; i < 30; i++) {
-    if (bal[i].y <= y && bal[i].y >= 20) {
+function mousehovered(x, y) {
+     noStroke();
+     fill(255, 20, 189);
+     rect(x - 50, y-10, 100, 20);
+  for (var i = 0; i < bal.length; i++) {
+    if (bal[i].y < y && bal[i].y > y - 30 && bal[i].x < x + 50 && bal[i].x > x - 50) {
       bal[i].dy = -abs(bal[i].dy) * GRAVITY;
-    } else if (bal[i].y > y && bal[i].y <= wy) {
+    } else if (bal[i].y >= y && bal[i].y < y + 30 && bal[i].x < x + 50 && bal[i].x > x - 50) {
       bal[i].dy = abs(bal[i].dy) * GRAVITY;
     }
 
+    // if (bal[i].x < x && bal[i].x > x - 15) {
+    //   bal[i].dx = -abs(bal[i].dx) * GRAVITY;
+    // } else if (bal[i].x >= x && bal[i].x < x + 15) {
+    //   bal[i].dx = abs(bal[i].dx) * GRAVITY;
+    // }
+  }
+}
+
+function mousepressed(x, y) {
+  for (var i = 0; i < bal.length; i++) {
+    if (bal[i].y <= y && bal[i].y >= 20) {
+      bal[i].dy = -abs(bal[i].dy) * ACC;
+    } else if (bal[i].y > y && bal[i].y <= wy) {
+      bal[i].dy = abs(bal[i].dy) * ACC;
+    }
+
     if (bal[i].x <= x && bal[i].x >= 20) {
-      bal[i].dx = -abs(bal[i].dx) * GRAVITY;
+      bal[i].dx = -abs(bal[i].dx) * ACC;
     } else if (bal[i].x > x && bal[i].x <= wx) {
-      bal[i].dx = abs(bal[i].dx) * GRAVITY;
+      bal[i].dx = abs(bal[i].dx) * ACC;
     }
   }
 }
@@ -101,17 +122,20 @@ function draw() {
   if(mouseIsPressed) {
     mousepressed(mouseX, mouseY);
   }
-   trail.push([mouseX, mouseY]);
-   for (let i = 0; i < trail.length; i++) {
-     noStroke();
-     fill(255, 20, 189, a);
-     ellipse(trail[i][0], trail[i][1], i);
-     if (a > 255) {
-       trail.shift();
-       a = 0;
-     }
-     a += 8;
-   }
+  //  trail.push([mouseX, mouseY]);
+  //  for (let i = 0; i < trail.length; i++) {
+  //    noStroke();
+  //    fill(255, 20, 189, a);
+  //    rect(trail[i][0] - 50, trail[i][1], i + 80, i + 2);
+  //   //  ellipse(trail[i][0], trail[i][1], i);
+  //    if (a > 255) {
+  //      trail.shift();
+  //      a = 0;
+  //    }
+  //    a += 8;
+  //  }
+
+   mousehovered(mouseX, mouseY);
 }
 
 function windowResized() {
